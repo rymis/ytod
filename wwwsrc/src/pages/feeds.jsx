1a5214f56302c63d275998aa84dc222463347321
Natalia Polikashina <polikashina@gmail.com>
2025-01-19 22:12:11 +0100

    fix tests

--- wwwsrc/src/pages/feeds.jsx
+++ wwwsrc/src/pages/feeds.jsx
@@ -1,6 +1,5 @@
 import m from "mithril";
 import { Item } from "../ui/item.jsx";
-import { Filter } from "../ui/filter.jsx";
 
 export let Feeds = {
     feeds: [],
@@ -15,7 +14,6 @@ export let Feeds = {
         allItems.sort((i1, i2) => i2.time - i1.time);
         return (
             <div>
-                <Filter feeds={Feeds.feeds} />
                 <div class="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
                     {allItems.map(item => <Item item={item}></Item>)}
                 </div>
