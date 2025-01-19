0d34ecc056f483cea3e2ce01bca0d004820842e4
Mikhail Ryzhov <rymiser@gmail.com>
2025-01-19 12:36:18 +0100

    Integration tests

--- Makefile
+++ Makefile
@@ -4,8 +4,13 @@ all:
 format:
 	python3 -m pycodestyle --ignore=E722 --max-line-length=200 ytod/*.py
 
-test:
-	python3 -m unittest ytod/*_test.py
+test: unittest integration_test
+	echo "All tests have passed"
 
+unittest:
+	./venv/bin/python3 -m unittest ytod/*_test.py
 
-.PHONY: format test
+integration_test:
+	./venv/bin/python3 ./integration_test/integration_test.py test
+
+.PHONY: format test integration_test unittest
--- integration_test/integration_test.py
+++ integration_test/integration_test.py
@@ -9,6 +9,8 @@ import random
 import tempfile
 import shutil
 import zipfile
+import requests
+import signal
 
 
 PROCS = []
@@ -21,14 +23,14 @@ def close_all():
         try:
             x = p.poll()
             if x is None:
-                p.kill()
+                p.send_signal(signal.SIGTERM)
                 p.wait()
         except:
             print("Can not kill process: {p.pid}")
     shutil.rmtree(WORKDIR, ignore_errors=True)
 
 
-def main():
+def run_stack():
     os.putenv("YTOD_OVERRIDE_YOUTUBE_URL", "http://localhost:18321/rss?channel_id=")
     atexit.register(close_all)
 
@@ -49,8 +51,104 @@ def main():
     p = sp.Popen([sys.executable, "-m", "ytod", "-d", "-w", WORKDIR], cwd=os.path.join(ROOT, ".."))
     PROCS.append(p)
 
-    print("WAITING...")
-    p.wait()
+    return p
+
+
+def test_auth():
+    data = requests.get("http://localhost:12345/ytod/api/user_info", auth=requests.auth.HTTPBasicAuth("test", "12345678")).json()
+    assert data["name"] == "test"
+
+    # Check incorrect auth:
+    resp = requests.get("http://localhost:12345/ytod/api/user_info", auth=requests.auth.HTTPBasicAuth("test", "12345687"))
+    assert not resp.ok
+
+    resp = requests.get("http://localhost:12345/ytod/api/user_info", auth=requests.auth.HTTPBasicAuth("user", "12345678"))
+    assert not resp.ok
+
+    resp = requests.get("http://localhost:12345/ytod/api/user_info", headers={"Remote-User": "user"})
+    assert not resp.ok
+
+
+def GET(method, params=None):
+    return requests.get(f"http://localhost:12345/ytod/api/{method}", params=params, auth=requests.auth.HTTPBasicAuth("test", "12345678"))
+
+
+def POST(method, data):
+    return requests.post(f"http://localhost:12345/ytod/api/{method}", json=data, auth=requests.auth.HTTPBasicAuth("test", "12345678"))
+
+
+def run_test():
+    test_auth()
+    # Operation:
+    info = GET("user_info").json()
+    assert len(info["feeds"]) == 0
+
+    # Search for videos:
+    srch = GET("search", {"q": "search query"}).json()
+    channels = []
+
+    for v in srch["items"]:
+        channels.append(v["channel_id"])
+
+    GET("subscribe", {"channel_id": channels[0]})
+    GET("subscribe", {"channel_id": channels[1]})
+
+    # And now we want to check subscriptions:
+    feeds = GET("feeds").json()
+    assert channels[0] in feeds
+    assert channels[1] in feeds
+
+    videos = []
+    for _, feed in feeds.items():
+        for v in feed["items"]:
+            videos.append(v)
+
+    # Download one video:
+    assert POST("download", {"video_id":videos[0]["video_id"]}).ok
+
+    for i in range(100):
+        print("Waiting for the video...")
+        if len(GET("local_videos").json()["result"]) > 0:
+            break
+        time.sleep(5)
+
+    # Check video in archive
+    archive = GET("local_videos").json()["result"]
+    assert len(archive) == 1
+    assert archive[0]["link"] == videos[0]["link"]
+    assert archive[0]["watch"]
+    assert GET("image", {"url": archive[0]["thumbnail"]["url"]}).ok
+
+    # Remove video:
+    assert GET("remove", {"video_id": archive[0]["watch"]})
+    archive = GET("local_videos").json()["result"]
+    assert len(archive) == 0
+
+    # Check unsubscribe:
+    GET("unsubscribe", {"channel_id": channels[0]})
+    feeds = GET("feeds").json()
+    assert channels[0] not in feeds
+    assert channels[1] in feeds
+
+
+def main():
+    p = run_stack()
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        time.sleep(2)
+        res = 0
+        try:
+            run_test()
+        except:
+            res = 1
+        p.send_signal(signal.SIGTERM)
+        time.sleep(1)
+        p.wait()
+        print(f"Test result: {res}")
+        sys.exit(res)
+    else:
+        print("WAITING...")
+        p.wait()
 
 
 if __name__ == '__main__':
